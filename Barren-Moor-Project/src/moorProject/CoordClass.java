07d244224c502d5aa3a3068dcce076c4d1df745d
Adam <adamjrate@hotmail.co.uk>
2017-07-28 10:58:09 +0000

    Added 'Soundtrack'

--- Barren-Moor-Project/src/moorProject/CoordClass.java
+++ Barren-Moor-Project/src/moorProject/CoordClass.java
@@ -1,26 +1,74 @@
 package moorProject;
 
 public class CoordClass {
+	int playerXValue, goalXValue;
+	int playerYValue, goalYValue;
+	int rows = 7;
+	int columns = 7;
+	int[][] gridPos = new int[columns][rows];
 
-	public static void createGridPositions(){
-		int rows = 7;
-		int columns = 7;
-		int [][] gridPos = new int [columns][rows];
-		int xValue = 0;
-		int yValue =0;
+	public int createGridPositions() {
+		playerXValue = 3;
+		playerYValue = 3;
+		goalXValue = 0;
+		goalYValue =0;
+		gridPos[playerXValue][playerYValue] = 1;
 		
-		gridPos[3][3] = 1;
-		
-		for(int i = 0; i < gridPos.length; i++){
+
+		for (int i = 0; i < gridPos.length; i++) {
 			System.out.println("");
-			for(int j = 0; j < gridPos[0].length; j++){
-				
-				System.out.print(gridPos[i][j]);
+			for (int j = 0; j < gridPos[0].length; j++) {
+
+				System.out.print("[" + gridPos[i][j] + "]" + " ");
 			}
-			//System.out.println(gridPos[i][j]);
 		}
-		
+		System.out.println("\n");
+		return playerXValue + playerYValue;
+	}
+
+	public int getPlayerXValue() {
+		return playerXValue;
+	}
+
+	public int getPlayerYValue() {
+		return playerYValue;
+	}
+
+	public int getDestXValue(){
+		return goalXValue;
 	}
 	
+	public int getDestYValue(){
+		return goalYValue;
+	}
 	
+	public void moveNorth() {
+		int gotX = getPlayerXValue();
+		int gotY = getPlayerYValue();
+		gridPos[gotX][gotY] = 0;
+		gridPos[gotX - 1][gotY] = 1;
+	}
+
+	public void moveEast() {
+		int gotX = getPlayerXValue();
+		int gotY = getPlayerYValue();
+		gridPos[gotX][gotY] = 0;
+		gridPos[gotX][gotY + 1] = 1;
+	}
+
+	public void moveSouth() {
+		int gotX = getPlayerXValue();
+		int gotY = getPlayerYValue();
+		gridPos[gotX][gotY] = 0;
+		gridPos[gotX + 1][gotY] = 1;
+
+	}
+
+	public void moveWest() {
+		int gotX = getPlayerXValue();
+		int gotY = getPlayerYValue();
+		gridPos[gotX][gotY] = 0;
+		gridPos[gotX][gotY - 1] = 1;
+	}
+
 }
--- Barren-Moor-Project/src/moorProject/RunnerClass.java
+++ Barren-Moor-Project/src/moorProject/RunnerClass.java
@@ -3,8 +3,32 @@ package moorProject;
 public class RunnerClass {
 
 	public static void main(String[] args) {
-
-		CoordClass.createGridPositions();
+		
+		System.out.println("You find yourself in the middle of a swamp");
+		//playSound.play();
+		System.out.println("Try using different commands to escape");
+		
+		CoordClass c = new CoordClass();
+		c.createGridPositions();
+		int playerX = c.getPlayerXValue();
+		int playerY = c.getPlayerYValue();
+		int destX = c.getDestXValue();
+		int destY = c.getDestYValue();
+		
+		
+		//boolean playing = true;
+		//while (playing != false){
+		//System.out.println("You are " + Pythagoras dX and dY + " Away from the objective");
+			//Update Player on surroundings/distance away
+			//Read User input
+			//Handle User input
+		//}
+		c.moveWest();
+		c.moveWest();
+		c.moveWest();
+		c.moveNorth();
+		c.moveNorth();
+		c.moveNorth();
 		
 	}
 
