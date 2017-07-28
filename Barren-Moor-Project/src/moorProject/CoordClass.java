0297e64a3d22f6c7232f6fb2e8d2204a0087ce6f
Adam <adamjrate@hotmail.co.uk>
2017-07-28 09:45:55 +0000

    Play Grid Created

--- Barren-Moor-Project/src/moorProject/CoordClass.java
+++ Barren-Moor-Project/src/moorProject/CoordClass.java
@@ -2,4 +2,25 @@ package moorProject;
 
 public class CoordClass {
 
+	public static void createGridPositions(){
+		int rows = 7;
+		int columns = 7;
+		int [][] gridPos = new int [columns][rows];
+		int xValue = 0;
+		int yValue =0;
+		
+		gridPos[3][3] = 1;
+		
+		for(int i = 0; i < gridPos.length; i++){
+			System.out.println("");
+			for(int j = 0; j < gridPos[0].length; j++){
+				
+				System.out.print(gridPos[i][j]);
+			}
+			//System.out.println(gridPos[i][j]);
+		}
+		
+	}
+	
+	
 }
--- Barren-Moor-Project/src/moorProject/RunnerClass.java
+++ Barren-Moor-Project/src/moorProject/RunnerClass.java
@@ -4,6 +4,8 @@ public class RunnerClass {
 
 	public static void main(String[] args) {
 
+		CoordClass.createGridPositions();
+		
 	}
 
 }
